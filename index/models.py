c2952596e3039dd4f8d1e58c2542c8076128c5e9
xyz25 <1241330802@qq.com>
2020-04-07 21:31:09 +0800

    完成admin-model

--- index/models.py
+++ index/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Admin(models.Model):
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'admin'
