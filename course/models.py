e1e9b79c446f3f7687dd45cf7c0b75c3bb1c5a6c
xyz25 <1241330802@qq.com>
2020-04-07 21:45:57 +0800

    完成count-model

--- course/models.py
+++ course/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=20)
+    cate = models.BooleanField()
+    user_id = models.IntegerField()
+
+    class Meta:
+        db_table = 'course'
+
+
+class Count(models.Model):
+    title = models.CharField(max_length=20)
+    counts = models.IntegerField()
+    last_time = models.DateTimeField(auto_now_add=True)
+    course_id = models.IntegerField()
+
+    class Meta:
+        db_table = 'count'
