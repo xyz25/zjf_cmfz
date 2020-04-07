a8fc671672477e32cca80624583cd008b4672985
xyz25 <1241330802@qq.com>
2020-04-07 21:52:11 +0800

    完成article-model

--- article/models.py
+++ article/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    author = models.CharField(max_length=20)
+    publish_time = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    pics = models.CharField(max_length=20)
+    cate = models.BooleanField()
 
+    class Meta:
+        db_table = 'article'
