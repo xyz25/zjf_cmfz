4a7b18b09fc60cc6a627397872ece5fd677930d2
xyz25 <1241330802@qq.com>
2020-04-07 21:44:31 +0800

    完成count-model

--- article/models.py
+++ article/models.py
@@ -6,3 +6,17 @@ class Course(models.Model):
     title = models.CharField(max_length=20)
     cate = models.BooleanField()
     user_id = models.IntegerField()
+
+    class Meta:
+        db_table = 'course'
+
+
+class Count(models.Model):
+    title = models.CharField(max_length=20)
+    counts = models.IntegerField()
+    last_time = models.DateTimeField(auto_now_add=True)
+    course_id = models.IntegerField()
+
+    class Meta:
+        db_table = 'count'
+    
